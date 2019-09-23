3a3cd9591f781d3e7272b4853612a7ffb268fd11
zerkijulian92 <zerkijuliandri92@gmail.com>
2019-09-23 12:07:08 +0700

    ItemCardViewAdapter.java | Mendeklarasikan

--- app/src/main/java/com/takatutustudio/projectrecyclerview/adapter/ItemCardViewAdapter.java
+++ app/src/main/java/com/takatutustudio/projectrecyclerview/adapter/ItemCardViewAdapter.java
@@ -1,4 +1,32 @@
 package com.takatutustudio.projectrecyclerview.adapter;
 
-public class ItemCardViewAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ItemCardViewAdapter extends RecyclerView.Adapter<ItemCardViewAdapter.CardViewHolder> {
+
+    @NonNull
+    @Override
+    public CardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CardViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class CardViewHolder extends RecyclerView.ViewHolder {
+        public CardViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
